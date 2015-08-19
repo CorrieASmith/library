42a57f071ff865d72ac02acf43cbcdaab08c1a18
Corrie Smith & Michael Summers <student@epicodus.com>
2015-08-19 09:55:54 -0700

    Include spec to test attr reader

--- spec/books_spec.rb
+++ spec/books_spec.rb
@@ -7,4 +7,11 @@ describe(Books) do
       expect(Books.all()).to(eq([]))
     end
   end
+
+  describe('#name') do
+    it('outputs name') do
+      test_book = Books.new({:name => "1984", :author => "George Orwell", :genre => "dystopian", :publisher => "Secker & Warburg", :year => "1949", :quantity => 12, :id => nil})
+      expect(test_book.name()).to eq("1984")
+    end
+  end
 end
